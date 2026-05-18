1b640d2b60c6b7ccf1dada07ca13978862ee450f
ziabaa <y1vd196@gmail.com>
2026-05-19 00:57:45 +0300

    Добавьте файлы проекта.

---
+++ SemanticSearch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SemanticSearch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
